9d9b483fa64cda6aef907f7bb6ae2d1238591255
Morel <MorelCisse19@gmail.com>
2017-07-19 09:58:43 +0000

    Page Login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Morel Cisse
                 </div>
 
                 <div class="links">
